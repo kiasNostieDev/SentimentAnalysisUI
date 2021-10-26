233638ed0ae8273336465e037d2812d327304be3
kiasNostieDev <akinthiyasrinath@gmail.com>
2021-10-26 12:06:59 +0530

    home page with worst react lines

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import { w3cwebsocket as W3CWebSocket } from "websocket"
 import { useState } from 'react'
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material/styles'
+import { IconButton } from '@mui/material'
+import BubbleChartIcon from '@mui/icons-material/BubbleChart'
+
+const Input = styled('input')({
+  display: 'none',
+});
 
 const client = new W3CWebSocket('ws://127.0.0.1:8000')
 function App() {
@@ -21,7 +29,24 @@ function App() {
       <div className='whoarewe'>ivies</div>
       <div className='whatplace'><div className='whatwedo'>hate Classification on Social Media Content</div></div>
       <div className='elaborate'>We provide this microservice based application to intelligently identify hate content on any deployed media.</div>
-      <div className='instantplace'></div>
+      <div className='instantplace'>
+        <div className='checkTitle'>Instant Score Check</div>
+        <textarea className='sendPut' placeholder='Text Goes Here' />
+        <div>
+          <div className='sendIns'>You can also manually and instantly check some data and analyse based on our service. Text goes above and Files go below(Images/Video)</div>
+          <label htmlFor="contained-button-file">
+            <Input id="contained-button-file" type="file" />
+            <Button style={{
+              width: '80%', marginLeft: '9.5%', marginTop: '5%', backgroundColor: 'rgba(131, 64, 148, 0.185)'
+            }} variant="contained" component="span">
+              file Upload
+            </Button>
+          </label>
+        </div>
+        <IconButton aria-label="delete" style={{ display: 'block', margin: 'auto' }}>
+          <BubbleChartIcon sx={{ color: 'red' }} style={{fontSize: '40pt', backgroundColor: 'rgba(131, 64, 148, 0.185)', borderRadius: '25%', marginTop: '30%'}} />
+        </IconButton>
+      </div>
     </div>
   );
 }
